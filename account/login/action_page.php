8b7900da32e971dc46ada67023d253f504a2da12
Daniel McKeown <dj@sf3am.com>
2023-05-25 18:40:32 -0700

    remove variable that causes warning

--- account/login/action_page.php
+++ account/login/action_page.php
@@ -2,14 +2,11 @@
 require '../../post/connect.php';
 include '../../post/postHeader.php';
 
-//$currentDate = new date();
 $content = $date_now = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $content = $_POST["content"];
-  //$date_now = $currentDate->format('Y-m-d H:i:s');
+  //  $content = $_POST["content"];
 }
-
 
 // set parameters and execute
 $poster = $_POST["fname"];
@@ -17,7 +14,6 @@ $raw_pw = $_POST["pwd"];
 
 $password_hash = password_hash($raw_pw, PASSWORD_DEFAULT);
 
-//  $sql = "SELECT poster, uu_id FROM Users WHERE password_hash='" . "$password_hash" . "'"; 
 $sql = "SELECT poster, password_hash, uu_id FROM Users WHERE poster='" . "$poster" . "'"; 
 
 $result = $conn->query($sql);
@@ -29,11 +25,8 @@ $uu_id_result = '';
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    //  echo  "poster: " . $row["poster"] .  " | " . $row["uu_id"] . "<br>";
     echo "login found <br />";
-  //  echo "password entered, hashed: " . $password_hash . "<br />  |   password in database: " . $row["password_hash"];
-
-    //if ($row["password_hash"] == $password_hash) {
+    
     if (password_verify($raw_pw, $row["password_hash"])) {
       echo "<br /><b>password correct</b>" . "<br /><a href='../../post'>create post</a>";
 
